a01c07a07c19be6ed20ebf290b356bf26bd7bf5b
CherylGoody <you@example.com>
2023-03-17 16:19:53 -0700

    Patience, persistence and perspiration make an unbeatable combination for success; one great lesson ALX course is teaching me

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - startup program
+ *
+ * Return: 0 successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		putchar(i + '0');
+		if (i != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
