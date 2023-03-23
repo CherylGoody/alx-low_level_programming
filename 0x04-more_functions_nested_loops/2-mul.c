29c7ab148706df85eaa8ea2bef77f377f108daea
CherylGoody <you@example.com>
2023-03-23 16:15:02 -0700

    multiplication is all about collaboration

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *mul - function that multiplies 2 numbers
+ *
+ *@a: first parameter to be multiplied
+ *@b: second parameter to be multiplied
+ *
+ *Return: result of the multiplicatiom
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
