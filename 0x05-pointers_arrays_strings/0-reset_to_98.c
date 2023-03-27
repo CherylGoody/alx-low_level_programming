4bccdb45fdd8430db1806bfbdcdf91ef7fa41a2a
CherylGoody <you@example.com>
2023-03-27 07:47:42 -0700

    pointers are vital in pogramming

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  reset inputed number to 98
+ *
+ * @n: holds number to be changed
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
+
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,2 @@
+Project on pointers, arrays and strings
+C - Pointers, arrays and strings
