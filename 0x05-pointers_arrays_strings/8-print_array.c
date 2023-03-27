f0dbdf6796f1b4d336fe2d126bc8fef5dce6dd83
CherylGoody <you@example.com>
2023-03-27 13:34:33 -0700

    Pointers are different from Arrays

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - function that prints n elements of an array of integers,
+ *followed by a new line
+ *
+ *@a:parameter that takes values
+ *@n: parameter that takes values
+ *
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < (n - 1); i++)
+	{
+		printf("%d, ", a[i]);
+	}
+	if (i == (n - 1))
+	{
+		printf("%d", a[n - 1]);
+	}
+	printf("\n");
+}
