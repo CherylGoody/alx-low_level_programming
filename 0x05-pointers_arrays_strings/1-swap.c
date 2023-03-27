f78321947955870c7bcfd261b03a18782a1df23e
CherylGoody <you@example.com>
2023-03-27 07:57:42 -0700

    Numbers can be swapped

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *swap_int - function that swaps two numbers
+ *
+ *@a: first value to be swapped
+ *@b: second value to be swapped
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
